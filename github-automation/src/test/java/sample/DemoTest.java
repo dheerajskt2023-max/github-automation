cc6f707ae45569567afabc602f6bc437c8183279
dheerajskt2023-max <dheerajskt2023@gmail.com>
2026-09-01 00:16:40 +0530

    abcd

--- github-automation/src/test/java/sample/DemoTest.java
+++ github-automation/src/test/java/sample/DemoTest.java
@@ -4,5 +4,8 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("hi there");
 		System.out.println("good night ");
+		System.out.println("hi there");
+		System.out.println("good night ");
+		
 	}
 }
